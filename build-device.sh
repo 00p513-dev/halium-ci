ab2538c6d318656914f930e16a66aebba8dd2b29
Jonah Brüchert <jbb.prv@gmx.de>
2018-10-07 21:31:49 +0200

    repo: be more quiet

--- build-device.sh
+++ build-device.sh
@@ -6,7 +6,7 @@ PRODUCT=$2
 # Seems like we can't share this step between the different jobs :/
 mkdir halium && cd halium
 repo init -u https://github.com/Halium/android -b halium-7.1 --depth=1
-repo sync -c -j 16
+repo sync -q -c -j 16
 
 # Actually start building
 source build/envsetup.sh
