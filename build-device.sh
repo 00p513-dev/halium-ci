1b1c22da1a7430b0f35fcff7b549d7b0ac89c5b2
Jonah Brüchert <jbb.prv@gmx.de>
2018-10-07 19:38:20 +0200

    less pipeline stuff

--- build-device.sh
+++ build-device.sh
@@ -3,7 +3,12 @@
 DEVICE=$1
 PRODUCT=$2
 
-cd halium
+# Seems like we can't share this step between the different jobs :/
+mkdir halium && cd halium
+repo init -u https://github.com/Halium/android -b halium-7.1 --depth=1
+repo sync -c -j 16
+
+# Actually start building
 source source build/envsetup.sh
 ./halium/devices/setup $DEVICE
 breakfast $PRODUCT
